7934956463b77615b01dc6faab3239e82a3ef065
zengyu <zeng_yu_1213@126.com>
2021-02-19 15:40:55 +0800

    [ADD] play string and puts.

---
+++ fun_with_str.rb
@@ -0,0 +1,19 @@
+# can use " and ', but ' is more efficient
+puts "Hello world"
+puts 'Hello world'
+
+puts 'I like' + ' ruby'
+
+puts 'It\'s my ruby'
+
+puts 'Hello' * 3
+
+PI = 3.1416
+puts PI
+
+# ruby call to_s first to 1213
+# like puts(1213.to_s())
+puts 1213
+
+# exec command, ths dir will be send to os
+puts `dir`
